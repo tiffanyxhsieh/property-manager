2822b480198d0fab1d092fbb32ed8f23e48a6340
Tiffany <tiffanyxhsieh@gmail.com>
2018-06-15 15:20:09 -0400

    added logout from home

--- propertymanager/settings.py
+++ propertymanager/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+LOGIN_REDIRECT_URL = '/'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'propertymanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
