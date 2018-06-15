1fa404ef6ae9b9628a7c66b41f9ce70960a068fb
Tiffany <tiffanyxhsieh@gmail.com>
2018-06-14 21:59:23 -0400

    changed customer to be extended from user

--- customer_account/models.py
+++ customer_account/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Customer(models.Model):
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.IntegerField()
     date_of_birth = models.DateField()
 
--- property/models.py
+++ property/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from housing_application.models import HousingApplication
+from customer_account.models import Customer
 # Create your models here.
 
 
 class Property(models.Model):
     address = models.CharField(max_length=250)
     housing_application = models.ForeignKey(HousingApplication, on_delete=models.PROTECT)
+    occupied_by = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
