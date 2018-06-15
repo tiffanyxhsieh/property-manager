7bdb098daa35dc3a533926a25d91e10ee6c64fa2
Tiffany <tiffanyxhsieh@gmail.com>
2018-06-15 18:46:49 -0400

    added basic registration

--- propertymanager/urls.py
+++ propertymanager/urls.py
@@ -22,6 +22,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('customer_account.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 
 ]
