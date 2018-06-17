dd4fe2635c56cd5519f374bfd39329c96be64533
Tiffany <tiffanyxhsieh@gmail.com>
2018-06-17 11:30:12 -0400

    fixed logout url redirect. added sign up link to home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView
+from django.shortcuts import redirect
 
 
 urlpatterns = [
     path('signup/', views.customer_signup, name='signup'),
+
 ]
