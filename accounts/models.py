52691512bcd6a502bd67729b477c066b70a34cc5
Tiffany <tiffanyxhsieh@gmail.com>
2018-06-24 12:33:18 -0400

    added localflavor. Changed Property model to include street address, state, zip code using localflavor

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-#TODO: change app to just "accounts"
-#TODO: add "staff" and "admin/manager" users
-
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,7 +8,6 @@ class Customer(models.Model):
     date_of_birth = models.DateField()
 
 
-#TODO: split into leasing employee and maintenance employee?
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.CharField(max_length=10)
--- property/models.py
+++ property/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from housing_application.models import HousingApplication
 from accounts.models import Customer
+from localflavor.us.models import USZipCodeField, USStateField
 # Create your models here.
 
 
 class Property(models.Model):
-    address = models.CharField(max_length=250)
+    street_address = models.CharField(max_length=100, default='0')
+    state = USStateField
+    zip_code = USZipCodeField
     housing_application = models.ForeignKey(HousingApplication, on_delete=models.PROTECT)
     occupied_by = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     publicly_available = models.BooleanField(default=False)
