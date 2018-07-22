ab696fab9d7216094722be5596edf982f4546036
Tiffany <tiffanyxhsieh@gmail.com>
2018-07-21 22:03:42 -0400

    added user fields in admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Employee
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Customer, Employee, User
+from django.contrib.auth.forms import UserCreationForm
 
+admin.site.unregister(User)
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = ('username', 'first_name', 'last_name','password1','password2')
+
+
+class MyUserAdmin(BaseUserAdmin):
+    add_form = MyUserCreationForm
+
+# don't forget to register your custom admin class
+admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(Employee)
+
+
